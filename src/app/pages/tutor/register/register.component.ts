7d1a7e9a794c0384c91e17b2d773fd9b4b2cd351
Alfredo Dominguez <thecatura.ad@gmail.com>
2024-06-08 12:01:25 -0300

    register form working good

--- src/app/pages/tutor/register/register.component.ts
+++ src/app/pages/tutor/register/register.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
-  Validators
+  Validators,
+  FormControl
 } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
@@ -13,37 +14,61 @@ import { HttpClient } from '@angular/common/http';
 })
 export class RegisterComponent implements OnInit {
   registrationForm: FormGroup | any;
-  register_endpoint = 'localhost:8080/api/v1/tutor/register';
+  register_endpoint = 'http://localhost:8080/api/v1/tutors/register';
+  form_submitted = false;
 
   constructor(private formBuilder: FormBuilder, private http: HttpClient) {
     // TODO: Fix the default country in the form
-    this.registrationForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      name: ['', Validators.required],
-      country: ['', Validators.required],
-      phone_number: ['', [Validators.required, Validators.pattern('[0-9]{10}')]],
-      birth_date: ['', Validators.required],
-      password: [
-        '',
-        [Validators.required,
-        Validators.pattern(
-          /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!#$%&?]).{8,32}$/
-        )]
-      ],
-      confirm_password: ['', Validators.required],
+    this.registrationForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      first_name: new FormControl('', Validators.required),
+      last_name: new FormControl('', Validators.required),
+      country: new FormControl('', Validators.required),
+      phone_number: new FormControl('', [Validators.required, Validators.pattern('[0-9]{10}')]),
+      birth_date: new FormControl('', Validators.required),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!#$%&?]).{8,32}$/)
+      ]),
+      confirm_password: new FormControl('', [Validators.required]),
     });
   }
-  submitForm(): void {
-    if (this.registrationForm?.valid) {
-      console.log('Form data:', this.registrationForm.value);
+  async submitForm() : Promise<void> {
+    if (this.registrationForm?.valid && this.passwordMatchValidator()) {
+      // TODO add country to the request
+      const request = {
+        ...this.registrationForm.value,
+      }
+      delete request.country;
+      delete request.confirm_password;
+      delete request.first_name;
+      delete request.last_name;
+      request.name = this.registrationForm.value.first_name;
+      request.surname = this.registrationForm.value.last_name;
+      const res = await fetch(this.register_endpoint, {
+        method: 'POST',
+        body: JSON.stringify(request),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      const data = await res.json();
+      if (data.status === 'success') {
+        this.form_submitted = true;
+        
+      } else {
+        // Tell user to try again
+        alert('An error occured, please try again later.');
+      }
+
     }
+
   }
 
-  test() {
-    console.log('Form data:', this.registrationForm.value);
-    console.log('Is valid: ', this.registrationForm.valid);
-  
+  passwordMatchValidator() {
+    return this.registrationForm.get('password').value === this.registrationForm.get('confirm_password').value
   }
+
   ngOnInit() {
     // const pwShowHide = document.querySelectorAll('.show-btn'),
     //   pwFields = document.querySelectorAll('.password'),
