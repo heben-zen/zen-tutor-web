7de6c58158f42dc21a3b15cd59b28dcceee06ebe
Alfredo Dominguez <thecatura.ad@gmail.com>
2024-06-10 20:17:30 -0300

    Put TCP port on prod URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,5 +9,5 @@ export const environment = {
     appId: "1:743793788639:web:3f2b8420da169f88810b6a",
     measurementId: "G-F6T5BZ42LB"
   },
-  API_URL: 'https://zen-tutor-web-api-2049284152.us-east-2.elb.amazonaws.com/api/v1'
+  API_URL: 'https://zen-tutor-web-api-2049284152.us-east-2.elb.amazonaws.com:8080/api/v1'
 };
