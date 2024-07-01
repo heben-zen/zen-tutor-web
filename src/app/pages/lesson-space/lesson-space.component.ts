48413bb22d4874f2f52e36c0f189018ed84c9df3
Alfredo Dominguez <thecatura.ad@gmail.com>
2024-07-01 18:50:34 -0300

    testing screensharing: properly triggering ended event

--- src/app/pages/lesson-space/lesson-space.component.ts
+++ src/app/pages/lesson-space/lesson-space.component.ts
@@ -121,7 +121,10 @@ export class LessonSpaceComponent implements AfterViewInit {
       }
     } else if (this.isCameraOn) { // Screen sharing is on and camera is on
       this.localVideo.nativeElement.srcObject = this.localStream;
-      this.localStreamScreen.getTracks().forEach(track => track.stop());
+      this.localStreamScreen.getTracks().forEach(track => {
+        track.stop()
+        track.dispatchEvent(new Event("ended"));
+      });
 
     } else { // Screen sharing is on and camera is off
       this.localVideo.nativeElement.srcObject = null;
