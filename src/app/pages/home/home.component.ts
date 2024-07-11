3017541bc587585f135476995f817a60b93e4252
Alfredo Dominguez <thecatura.ad@gmail.com>
2024-07-11 13:58:45 -0300

    fixed router link with new design pattern angular

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   standalone: true,
-  imports: [NavigationBarComponent]
+  imports: [NavigationBarComponent, RouterLink]
 })
 export class HomeComponent implements OnInit {
   title = 'Zen';
