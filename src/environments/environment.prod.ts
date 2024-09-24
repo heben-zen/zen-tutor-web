ba0c7ef93a148a012b9af2a40df8ecdc70140774
Alfredo Dominguez <thecatura.ad@gmail.com>
2024-09-24 16:32:58 -0300

    Updated firebase project id

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   firebaseConfig: {
     apiKey: "AIzaSyBz3Bc9wXNTwGPgxNJxkQwiWXKa1l7mbYw",
     authDomain: "zen-social-media.firebaseapp.com",
-    projectId: "zen-tutor-web",
+    projectId: "zen-ed",
     storageBucket: "zen-social-media.appspot.com",
     messagingSenderId: "743793788639",
     appId: "1:743793788639:web:3f2b8420da169f88810b6a",
