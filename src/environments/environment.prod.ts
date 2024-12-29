80d451d1fcf8dc370ba72b5c62750a62ad65eb01
Alfredo Dominguez <thecatura.ad@gmail.com>
2024-12-28 20:06:54 -0400

    Updated API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,8 +9,8 @@ export const environment = {
     appId: "1:743793788639:web:3f2b8420da169f88810b6a",
     measurementId: "G-F6T5BZ42LB"
   },
-  API_HOST: 'https://spring-api-ikmk7odgjq-uc.a.run.app:443',
-  API_URL: 'https://spring-api-ikmk7odgjq-uc.a.run.app:443/api/v1',
+  API_HOST: 'https://spring-api-229157258527.us-central1.run.app:443',
+  API_URL: 'https://spring-api-229157258527.us-central1.run.app:443/api/v1',
   VIDEO_WS_URI: 'wss://api.zentutor.ca',
   VIDEO_WS_PORT: '8081',
   MESSAGING_URI: 'https://api.zentutor.ca',
